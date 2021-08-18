18670696c6b21548bdcd3d4d1cfeabf61fbc281d
Alex <oleksii.petrochenko.pz.2019@lpnu.ua>
2021-08-18 12:52:36 +0300

    [PHP-5] add class Strings

---
+++ src/strings/Strings.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace strings;
+
+use strings\StringsInterface;
+
+class Strings implements StringsInterface
+{
+
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function snakeCaseToCamelCase(string $input): string
+    {
+        return str_replace('_', '', lcfirst(ucwords($input, '_')));
+    }
+
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function mirrorMultibyteString(string $input): string
+    {
+        $inputAsArray = explode(' ', $input);
+
+        foreach ($inputAsArray as &$word) {
+            $word = $this->utf8StringReverse($word);
+        }
+
+        return implode(' ', $inputAsArray);
+    }
+
+    /**
+     * @param string $noun
+     * @return string
+     */
+    public function getBrandName(string $noun): string
+    {
+        $result = "";
+
+        if ($noun[-1] === $noun[0]) {
+            $result = $noun . strstr($noun, $noun[1]);
+            $result = ucfirst($result);
+        } else {
+            $result = "The " . ucfirst($noun);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $input
+     * @return string
+     */
+    private function utf8StringReverse(string $input): string
+    {
+        preg_match_all('/./us', $input, $arr);
+        return join('', array_reverse($arr[0]));
+    }
+
+}
